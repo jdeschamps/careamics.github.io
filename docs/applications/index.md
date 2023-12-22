94707d4b098a46947c54ce2f6f02d61a3145ac66
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-12-22 11:57:34 +0100

    Fix notebook import

--- docs/applications/index.md
+++
@@ -1,7 +0,0 @@
----
-icon: octicons/file-media-24
-description: Applications
----
-# Applications
-
-This section contains example applications.
--- scripts/gen_jupyter_nav.py
+++ scripts/gen_jupyter_nav.py
@@ -1,4 +1,18 @@
-"""Generate navigation items for the jupyter notebooks."""
+"""Generate navigation items for the jupyter notebooks.
+
+The script runs in the root folder, but the relative paths need be built
+relative to the applications folder. Each call to "mkdocs_gen_files" is
+relative to the docs folder.
+
+We want to produce a SUMMARY.md file that looks like the following:
+* [Applications](index.md)
+* N2V
+    * [2D SEM](N2V/2D_SEM.ipynb)
+    * [3D SEM](N2V/3D_SEM.ipynb)
+* CARE
+    * [2D SEM](CARE/2D_SEM.ipynb)
+etc.
+"""
 from pathlib import Path
 
 import mkdocs_gen_files
@@ -8,35 +22,56 @@ def index_text():
             "icon: octicons/file-media-24\n" \
             "description: Applications\n" \
             "---\n" \
-            "# Applications\n\n" \
-            "Use the navigation index on the left to explore the applications."
+            "# Applications\n" \
+            "This section contains a list of example applications.\n\n"
 
 # source folder
-SRC = Path("applications")
-INDEX = SRC / "index.md"
+APP = Path("docs", "applications")
 
 # create mkdocs navigation
 nav = mkdocs_gen_files.Nav()
 
-# # create index file
-# with mkdocs_gen_files.open(INDEX, "w") as index_md:
-#     index_md.write(index_text())
+# create index file (relative to docs because we are calling mkdocs_gen_files)
+with mkdocs_gen_files.open(Path("applications", "index.md"), "w") as index_md:
+    index_md.write(index_text())
+nav[("Applications",)] = "index.md"
 
-# loop over all notebooks recursively 
-for path in SRC.rglob("*.ipynb"):
+# loop over all files, detect *.ipynb files and add them to the nav
+for path in APP.rglob("*.ipynb"):
     print(path)
 
-    # relatve path to applications
-    rel_path = path.relative_to(SRC).with_suffix("")
-    file_path = path.relative_to(SRC).with_suffix(".ipynb")
+    # get path
+    relative_path = path.relative_to(APP)
 
-    # parts
-    parts = tuple(rel_path.parts)
+    # get parts
+    parts = tuple(relative_path.parts)
+
+    # name without underscores
+    name = relative_path.stem.replace("_", " ")
+
+    # replace the last part with the name
+    parts = parts[:-1] + (name,)
 
     # add to navigation
-    nav[parts] = file_path.as_posix()
+    nav[parts] = relative_path.as_posix()
 
-# Write the navigation as a Markdown list in the literate navigation file
+# write the navigation as a Markdown list in the literate navigation file
 with mkdocs_gen_files.open("applications/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
+
+# read content of the SUMMARY.md file line by line, ommitting the first line
+with mkdocs_gen_files.open("applications/SUMMARY.md", "r") as nav_file:
+    lines = nav_file.readlines()[1:]
+
+    for i, line in enumerate(lines):
+        # change lines that start with a * to a markdown title
+        if line.startswith("*"):
+            lines[i] = f"## {line[2:]}\n"
+        # remove tab in the others
+        elif line.startswith("    "):
+            lines[i] = line[4:]
+
+    # write the lines into index.md
+    with mkdocs_gen_files.open("applications/index.md", "a") as index_md:
+        index_md.writelines(lines)
     
